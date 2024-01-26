a6cf5d0fa007e8d4cabd43acb09b02ec6b716c2d
Vadims Filipovskis <vadim.filipovsky@gmail.com>
2024-01-26 15:02:22 +0100

    cs

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,27 +1,6 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
-    build = vim.fn.has "win32" == 0
-        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-      or nil,
-    dependencies = { "rafamadriz/friendly-snippets" },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-      region_check_events = "CursorMoved",
-    },
-    config = require "plugins.configs.luasnip",
-  },
-  {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    event = "InsertEnter",
     opts = function()
       local cmp = require "cmp"
       local snip_status_ok, luasnip = pcall(require, "luasnip")
