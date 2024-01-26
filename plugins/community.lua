c105f91042c8b816e5731e54d3f0f77e1eaad4c7
Vadims Filipovskis <vadim.filipovsky@gmail.com>
2024-01-26 14:53:04 +0100

    spectre, git-blame, todo-comments

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.project/nvim-spectre" },
+  { import = "astrocommunity.editing-support/todo-comments-nvim" },
+  { import = "astrocommunity.git/git-blame-nvim" },
 }
